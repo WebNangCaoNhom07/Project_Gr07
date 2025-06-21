0a32d337deef75f7b967df904b5159e4d7513541
HoangPham <23010245@st.phenikaa-uni.edu.vn>
2025-06-21 11:49:25 +0700

    Cập nhật code

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,13 @@
 <?php
 
-use App\Http\Controllers\LaptopController;
-use App\Http\Controllers\ShowLaptopController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::view('/about', 'about')->name('about');
 
-// Route::controller(LaptopController :: class)->group(function(){
-//     Route::get('/laptop','index');
-//     Route::get('/My-laptop','myLaptop');
-// // });
-Route::get('/laptop/invokable', LaptopController::class);
-Route::get('/laptop', [LaptopController::class,'index']); 
-
-// routes/web.php
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
-//route for shop
-Route::get('/shop', function () {
-    return view('shop');
-})->name('shop');
-
+Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+Route::get('/shop', [PageController::class, 'shop'])->name('shop');
